35f9bcc27541bf1943ff873a28bf4ac8ef028ffe
Aya314 <ayashhadeh038@gmail.com>
2023-11-18 21:30:21 +0300

    login ui

--- src/pages/Auth/LoginPage.jsx
+++ src/pages/Auth/LoginPage.jsx
@@ -1,11 +1,14 @@
 import { Box } from '@mui/material';
 import React, { useState } from 'react';
+import { ThemeProvider } from '@material-ui/styles'; 
+import theme from './theme';
 
 const defaultTheme = createTheme();
 
 export default function LoginPage() {
 
         return (
+
                 <ThemeProvider theme={defaultTheme}>
                         <Box sx={{
                                 display: {
